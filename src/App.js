aa9d5ac93afd0a066f8d0a10c0a10521cd43d8af
ferrari219@nate.com <ferrari219@nate.com>
2021-06-21 13:29:41 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-class App extends Component {
-	state = {
-		data: [],
-	};
-
-	loadingData = async () => {
-		try {
-			const { data } = await axios.get('https://reqres.in/api/users?delay=1');
-			// const response = await axios.get('https://ferrari219.github.io/data1.json');
-			// const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-			this.setState({
-				data: data,
-			});
-		} catch (e) {
-			console.log(e);
-		}
-	};
-
-	componentDidMount() {
-		const { loadingData } = this;
-		loadingData();
-	}
-	render() {
-		return (
-			<div>
-				<p>{JSON.stringify(this.state.data.support && this.state.data.support.url)}</p>
-				<p>
-					<br />
-				</p>
-				{/* <p>{JSON.stringify(this.state.data.data[0].email)}</p> */}
-				{/* <p>{JSON.stringify(this.state.data.mainInfo)}</p> */}
-				{/* {mainInfo.map((item) => (
-					<test myname={item.myname} job={item.job} />
-				))} */}
-				{/* <p>{JSON.stringify(myname)}</p> */}
-				{/* <p>{JSON.stringify(this.state.data.subInfo.)}</p> */}
-				{/* <p>{JSON.stringify(data)}</p> */}
-			</div>
-		);
-	}
-}
+const App = () => {
+	return <div>App</div>;
+};
 
 export default App;
