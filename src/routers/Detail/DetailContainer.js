cba656c1242f94202f6374bd75305be9304a6f49
ferrari219@nate.com <ferrari219@nate.com>
2021-06-24 00:13:46 +0900

    제작중

--- src/routers/Detail/DetailContainer.js
+++ src/routers/Detail/DetailContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DetailPresenter from './DetailPresenter';
-
+// console.log();
 class DetailContainer extends Component {
 	render() {
 		return <DetailPresenter />;
