4252c859bf3a83d3f9b90ceb011195f3884077a1
ferrari219@nate.com <ferrari219@nate.com>
2021-06-23 17:19:09 +0900

    api제작

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+const api = axios.create({
+	baseURL: 'https://api.themoviedb.org/3/',
+	params: {
+		api_key: 'af8b05da74929c04bc7b06be136837ba',
+		language: 'en-us',
+	},
+});
+
+// api.get('tv/popular');
+
+export default api;
+export const movieApi = {
+	nowPlaying: () => api.get('movie/now_playing'),
+	upcoming: () => api.get('movie/upcoming'),
+	popular: () => api.get('movie/popular'),
+	movieDetail: (id) =>
+		api.get(`movie/${id}`, {
+			params: {
+				append_to_response: 'videos',
+			},
+		}),
+	search: (term) =>
+		api.get('search/movie', {
+			params: {
+				query: encodeURIComponent(term), //?
+			},
+		}),
+};
+export const tvApi = {
+	topRated: () => api.get('tv/top_rated'),
+	popular: () => api.get('tv/popular'),
+	airingToday: () => api.get('tv/airing_today'),
+};
