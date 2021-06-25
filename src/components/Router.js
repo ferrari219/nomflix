618d42be7375a6eb78885561e2c7e0c77e9426ad
ferrari219@nate.com <ferrari219@nate.com>
2021-06-25 17:58:19 +0900

    Done

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Home from 'routers/Home';
 import TV from 'routers/TV';
 import Search from 'routers/Search';
+import Detail from 'routers/Detail';
 
 const Router = () => {
 	return (
@@ -14,6 +15,8 @@ const Router = () => {
 					<Route path="/" exact component={Home} />
 					<Route path="/tv" component={TV} />
 					<Route path="/search" component={Search} />
+					<Route path="/movie/:id" component={Detail} />
+					<Route path="/show/:id" component={Detail} />
 					<Redirect form="*" to="/" />
 				</Switch>
 			</BrowserRouter>
