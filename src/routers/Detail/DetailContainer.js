0993ef677e6fd3a390608fa0453c723bf9e8cc81
ferrari219@nate.com <ferrari219@nate.com>
2021-06-27 01:31:21 +0900

    Detail #1

--- src/routers/Detail/DetailContainer.js
+++ src/routers/Detail/DetailContainer.js
@@ -2,8 +2,32 @@ import React, { Component } from 'react';
 import DetailPresenter from './DetailPresenter';
 // console.log();
 class DetailContainer extends Component {
+	state = {
+		result: null,
+		error: null,
+		loading: true,
+	};
+
+	componentDidMount() {
+		const {
+			match: {
+				params: { id },
+			},
+			history: { push },
+		} = this.props;
+		console.log(id);
+		const parseId = parseInt(id);
+		if (isNaN(parseId)) {
+			return push('/');
+		}
+	}
+
 	render() {
-		return <DetailPresenter />;
+		console.log(this.props);
+		const { result, error, loading } = this.state;
+		return (
+			<DetailPresenter result={result} error={error} loading={loading} />
+		);
 	}
 }
 
