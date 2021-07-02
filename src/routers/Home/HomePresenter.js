6f960425ceab826b5d7cef6b1a07db27dbde671e
ferrari219@nate.com <ferrari219@nate.com>
2021-07-02 12:53:51 +0900

    deploy

--- src/routers/Home/HomePresenter.js
+++ src/routers/Home/HomePresenter.js
@@ -9,52 +9,53 @@ const HomePresenter = ({ nowPlaying, upcoming, popular, loading, error }) => (
 		<Helmet>
 			<title>Movies | Nomflix</title>
 		</Helmet>
-		loading ? (
-		<Loader />) : (
-		<>
-			{nowPlaying && nowPlaying.length > 0 && (
-				<Sections title="nowPlaying">
-					{nowPlaying.map((item) => (
-						<Poster
-							key={item.id}
-							id={item.id}
-							imgUrl={item.poster_path}
-							title={item.original_title}
-							year={item.release_date.substring(0, 4)}
-							isMovie={true}
-						/>
-					))}
-				</Sections>
-			)}
-			{upcoming && upcoming.length > 0 && (
-				<Sections title="upcoming">
-					{upcoming.map((item) => (
-						<Poster
-							key={item.id}
-							id={item.id}
-							imgUrl={item.poster_path}
-							title={item.original_title}
-							year={item.release_date.substring(0, 4)}
-							isMovie={true}
-						/>
-					))}
-				</Sections>
-			)}
-			{popular && popular.length > 0 && (
-				<Sections title="popular">
-					{popular.map((item) => (
-						<Poster
-							imgUrl={item.poster_path}
-							title={item.original_title}
-							year={String(item.release_date).substring(0, 4)}
-							isMovie={true}
-						/>
-					))}
-				</Sections>
-			)}
-			<div>{JSON.stringify(nowPlaying)}</div>
-		</>
-		);
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				{nowPlaying && nowPlaying.length > 0 && (
+					<Sections title="nowPlaying">
+						{nowPlaying.map((item) => (
+							<Poster
+								key={item.id}
+								id={item.id}
+								imgUrl={item.poster_path}
+								title={item.original_title}
+								year={item.release_date.substring(0, 4)}
+								isMovie={true}
+							/>
+						))}
+					</Sections>
+				)}
+				{upcoming && upcoming.length > 0 && (
+					<Sections title="upcoming">
+						{upcoming.map((item) => (
+							<Poster
+								key={item.id}
+								id={item.id}
+								imgUrl={item.poster_path}
+								title={item.original_title}
+								year={item.release_date.substring(0, 4)}
+								isMovie={true}
+							/>
+						))}
+					</Sections>
+				)}
+				{popular && popular.length > 0 && (
+					<Sections title="popular">
+						{popular.map((item) => (
+							<Poster
+								imgUrl={item.poster_path}
+								title={item.original_title}
+								year={String(item.release_date).substring(0, 4)}
+								isMovie={true}
+							/>
+						))}
+					</Sections>
+				)}
+				<div>{JSON.stringify(nowPlaying)}</div>
+			</>
+		)}
 	</>
 );
 // console.log('HomePresenter nowPlaying:', nowPlaying);
--- src/routers/TV/TVPresenter.js
+++ src/routers/TV/TVPresenter.js
@@ -9,51 +9,61 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => (
 		<Helmet>
 			<title>TV Shows | Nomflix</title>
 		</Helmet>
-		loading ? (
-		<Loader />) : (
-		<>
-			{topRated && topRated.length > 0 && (
-				<Sections title="topRated">
-					{topRated.map((item) => (
-						<Poster
-							key={item.id}
-							id={item.id}
-							imgUrl={item.poster_path}
-							title={item.original_name}
-							year={String(item.first_air_date).substring(0, 4)}
-						/>
-					))}
-				</Sections>
-			)}
-			{popular && popular.length > 0 && (
-				<Sections title="popular">
-					{popular.map((item) => (
-						<Poster
-							key={item.id}
-							id={item.id}
-							imgUrl={item.poster_path}
-							title={item.original_name}
-							year={String(item.first_air_date).substring(0, 4)}
-						/>
-					))}
-				</Sections>
-			)}
-			{airingToday && airingToday.length > 0 && (
-				<Sections title="airingToday">
-					{airingToday.map((item) => (
-						<Poster
-							key={item.id}
-							id={item.id}
-							imgUrl={item.poster_path}
-							title={item.original_name}
-							year={String(item.first_air_date).substring(0, 4)}
-						/>
-					))}
-				</Sections>
-			)}
-			<div>{JSON.stringify(topRated)}</div>
-		</>
-		);
+		{loading ? (
+			<Loader />
+		) : (
+			<>
+				{topRated && topRated.length > 0 && (
+					<Sections title="topRated">
+						{topRated.map((item) => (
+							<Poster
+								key={item.id}
+								id={item.id}
+								imgUrl={item.poster_path}
+								title={item.original_name}
+								year={String(item.first_air_date).substring(
+									0,
+									4
+								)}
+							/>
+						))}
+					</Sections>
+				)}
+				{popular && popular.length > 0 && (
+					<Sections title="popular">
+						{popular.map((item) => (
+							<Poster
+								key={item.id}
+								id={item.id}
+								imgUrl={item.poster_path}
+								title={item.original_name}
+								year={String(item.first_air_date).substring(
+									0,
+									4
+								)}
+							/>
+						))}
+					</Sections>
+				)}
+				{airingToday && airingToday.length > 0 && (
+					<Sections title="airingToday">
+						{airingToday.map((item) => (
+							<Poster
+								key={item.id}
+								id={item.id}
+								imgUrl={item.poster_path}
+								title={item.original_name}
+								year={String(item.first_air_date).substring(
+									0,
+									4
+								)}
+							/>
+						))}
+					</Sections>
+				)}
+				<div>{JSON.stringify(topRated)}</div>
+			</>
+		)}
 	</>
 );
 // console.log('TVPresenter topRated:', topRated);
